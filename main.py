3ff77ff77547f7474b043d8f04f324afeb4bfdd7
SinGamePlay <singameplay992@gmail.com>
2025-06-27 17:30:49 +0200

    Primo commit - bot Discord

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    try:
+        synced = await bot.tree.sync()
+        print(f"✅ Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(f"❌ Sync failed: {e}")
+
+@bot.tree.command(name="ciao", description="Saluta il bot")
+async def ciao(interaction: discord.Interaction):
+    await interaction.response.send_message("Ciao anche a te!")
+
+if not token:
+    raise ValueError("TOKEN non definito")
+bot.run(token)
