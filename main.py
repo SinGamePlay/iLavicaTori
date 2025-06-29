963f934b1b02d66e15d0765287ed0d114cedbb59
JustEFFE <133548598+SinGamePlay@users.noreply.github.com>
2025-06-29 23:25:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.environ.get('TOKEN')
+token = os.environ.get('TOKEN')
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents)
